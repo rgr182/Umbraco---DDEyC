037aeb7cc6e0234153c7f31a7ab63ee5fa456b8f
Gibrán Pérez <109172763+Gapcroz@users.noreply.github.com>
2024-05-07 12:05:14 -0600

    update mobile Navigation bar responsive
    make a dropdown option in hamburguer menu

---
+++ DD/wwwroot/Scripts/mobileMenuToggle.js
@@ -0,0 +1,6 @@
+﻿const openMenuButton = document.querySelector('.open-menu');
+const navigationMenu = document.querySelector('#navigation ul');
+
+openMenuButton.addEventListener('click', () => {
+    navigationMenu.style.display = navigationMenu.style.display === 'none' ? 'block' : 'none';
+});
