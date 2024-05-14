eae78403644cb9e3c144efeb08946e22a9744047
Gibrán Pérez <109172763+Gapcroz@users.noreply.github.com>
2024-05-13 18:29:23 -0600

    Update activity DDEYC-7 and DDEYC-9
    display the options in navbar on mobile and show the example of searchbox ui/ux

--- DD/wwwroot/Scripts/mobileMenuToggle.js
+++ DD/wwwroot/Scripts/mobileMenuToggle.js
@@ -1,8 +1,5 @@
 ﻿const openMenuButton = document.querySelector('#open-menu');
 const navigationMenu = document.querySelector('#navigation ul');
-const navigation = document.querySelector('#navigation');
-const dropdownToggles = document.querySelectorAll('.has-dropdown .dropdown-arrow');
-const dropdownContents = document.querySelectorAll('.has-dropdown ul');
 
 openMenuButton.addEventListener('click', () => {
     navigationMenu.style.display = navigationMenu.style.display === 'none' ? 'block' : 'none';
@@ -11,28 +8,7 @@ openMenuButton.addEventListener('click', () => {
 window.addEventListener('resize', () => {
     if (window.innerWidth > 930) {
         navigationMenu.style.display = 'flex';
-    }
-});
-window.addEventListener('resize', () => {
-    if (window.innerWidth < 930) {
+    } else {
         navigationMenu.style.display = 'none';
     }
 });
-
-navigation.addEventListener('click', (event) => {
-    // Check if the click target is a dropdown arrow button
-    if (event.target.classList.contains('dropdown-arrow')) {
-        const toggle = event.target; // Get the clicked dropdown arrow button
-        const index = Array.from(navigation.querySelectorAll('.has-dropdown .dropdown-arrow')).indexOf(toggle); // Get the index of the clicked button
-
-        // Toggle the corresponding dropdown content
-        dropdownContents[index].classList.toggle('active');
-    }
-});
-
-if (dropdownToggles.length > 0 && dropdownContents.length > 0) {
-    // Proceed with event listener setup and dropdown functionality logic
-} else {
-    // Handle the case where no dropdown elements are found
-    console.error('Dropdown elements not found. Check HTML structure and class names.');
-}
