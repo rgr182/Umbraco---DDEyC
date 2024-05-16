a53cb4399bd9630738c8eefef3564b5724720e6e
Gibrán Pérez <109172763+Gapcroz@users.noreply.github.com>
2024-05-16 10:22:25 -0600

    Development  ddeyc79 ddeyc-76 ddeyc-74 (#20)
    Modify icon bar in navigation bar
    Mobile nav bar UI/UX improvements
    Desktop navbar bug fixes
    Change header images' position.
    Add a rainbow-box in button header.

--- DD/wwwroot/Scripts/mobileMenuToggle.js
+++ DD/wwwroot/Scripts/mobileMenuToggle.js
@@ -1,14 +1,48 @@
-﻿const openMenuButton = document.querySelector('#open-menu');
+﻿const dropdownArrows = document.querySelectorAll('.dropdown-arrow');
+const openMenuButton = document.querySelector('#open-menu');
 const navigationMenu = document.querySelector('#navigation ul');
 
+// Alternar la visibilidad del menú de navegación al hacer clic en el botón de menú
 openMenuButton.addEventListener('click', () => {
-    navigationMenu.style.display = navigationMenu.style.display === 'none' ? 'block' : 'none';
+    if (navigationMenu.style.display === 'none' || navigationMenu.style.display === '') {
+        navigationMenu.style.display = 'block';
+    } else {
+        navigationMenu.style.display = 'none';
+    }
 });
 
+// Alternar la visibilidad de los submenús al hacer clic en el dropdown-arrow
+dropdownArrows.forEach((arrow) => {
+    arrow.addEventListener('click', (e) => {
+        const dropdownMenu = arrow.nextElementSibling;
+        // Alternar la visibilidad del menú desplegable
+        if (dropdownMenu.style.display === 'block') {
+            dropdownMenu.style.display = 'none';
+        } else {
+            dropdownMenu.style.display = 'block';
+        }
+        e.stopPropagation(); // Evitar que el clic se propague a elementos superiores
+    });
+});
+
+// Ajustar la visibilidad del menú de navegación al redimensionar la ventana
 window.addEventListener('resize', () => {
     if (window.innerWidth > 930) {
         navigationMenu.style.display = 'flex';
     } else {
         navigationMenu.style.display = 'none';
+    }
+});
+
+// Ocultar el menú de navegación y los submenús si se hace clic fuera de ellos
+document.addEventListener('click', (e) => {
+    if (window.innerWidth <= 930 && navigationMenu && !e.target.closest('#navigation')) {
+        navigationMenu.style.display = 'none';
+        dropdownArrows.forEach((arrow) => {
+            const dropdownMenu = arrow.nextElementSibling;
+            if (dropdownMenu) {
+                dropdownMenu.style.display = 'none';
+            } 
+        });
     }
 });
