8f810663d7c11e467d9ede90f654ab608d192ea6
Daniel M. <83463344+Paroxyse@users.noreply.github.com>
2024-05-21 11:24:21 -0600

    Add hide toggle to Search Combos

--- DD/umbraco/models/LandingPage.generated.cs
+++ DD/umbraco/models/LandingPage.generated.cs
@@ -20,7 +20,7 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 {
 	/// <summary>Landing Page</summary>
 	[PublishedModel("landingPage")]
-	public partial class LandingPage : PublishedContentModel, IHeaderProperties, IHomeRedirectButtonProperties
+	public partial class LandingPage : PublishedContentModel, IHeaderProperties, IHomeRedirectButtonProperties, IRichTextFooterRow, IRichTextHeadlineRow, ISearchComboProperties
 	{
 		// helpers
 #pragma warning disable 0109 // new is redundant
@@ -58,6 +58,14 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockListItem<global::Umbraco.Cms.Web.Common.PublishedModels.VideoEmbedElement> EmbededVideo => this.Value<global::Umbraco.Cms.Core.Models.Blocks.BlockListItem<global::Umbraco.Cms.Web.Common.PublishedModels.VideoEmbedElement>>(_publishedValueFallback, "embededVideo");
 
 		///<summary>
+		/// Footer Headline Content
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.0.3+a0f3c15")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("footerHeadlineContent")]
+		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockListModel FooterHeadlineContent => this.Value<global::Umbraco.Cms.Core.Models.Blocks.BlockListModel>(_publishedValueFallback, "footerHeadlineContent");
+
+		///<summary>
 		/// Footer Items
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.0.3+a0f3c15")]
@@ -80,14 +88,6 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
 		[ImplementPropertyType("newsCarousel")]
 		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockListModel NewsCarousel => this.Value<global::Umbraco.Cms.Core.Models.Blocks.BlockListModel>(_publishedValueFallback, "newsCarousel");
-
-		///<summary>
-		/// News Headline: Enter main title to show news carousel
-		///</summary>
-		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.0.3+a0f3c15")]
-		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		[ImplementPropertyType("newsHeadline")]
-		public virtual global::Umbraco.Cms.Core.Strings.IHtmlEncodedString NewsHeadline => this.Value<global::Umbraco.Cms.Core.Strings.IHtmlEncodedString>(_publishedValueFallback, "newsHeadline");
 
 		///<summary>
 		/// Rich Text and Images
@@ -166,5 +166,80 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
 		[ImplementPropertyType("homeRedirectButtonURL")]
 		public virtual global::Umbraco.Cms.Core.Models.Link HomeRedirectButtonUrl => global::Umbraco.Cms.Web.Common.PublishedModels.HomeRedirectButtonProperties.GetHomeRedirectButtonUrl(this, _publishedValueFallback);
+
+		///<summary>
+		/// Footer Font Size
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.0.3+a0f3c15")]
+		[ImplementPropertyType("footerFontSize")]
+		public virtual int FooterFontSize => global::Umbraco.Cms.Web.Common.PublishedModels.RichTextFooterRow.GetFooterFontSize(this, _publishedValueFallback);
+
+		///<summary>
+		/// Footer Headline: Edit the headline title
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.0.3+a0f3c15")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("footerHeadline")]
+		public virtual global::Umbraco.Cms.Core.Strings.IHtmlEncodedString FooterHeadline => global::Umbraco.Cms.Web.Common.PublishedModels.RichTextFooterRow.GetFooterHeadline(this, _publishedValueFallback);
+
+		///<summary>
+		/// Footer Headline Color
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.0.3+a0f3c15")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("footerHeadlineColor")]
+		public virtual string FooterHeadlineColor => global::Umbraco.Cms.Web.Common.PublishedModels.RichTextFooterRow.GetFooterHeadlineColor(this, _publishedValueFallback);
+
+		///<summary>
+		/// Footer Headline Hide: Select to true if you need to hide the text
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.0.3+a0f3c15")]
+		[ImplementPropertyType("footerHeadlineHide")]
+		public virtual bool FooterHeadlineHide => global::Umbraco.Cms.Web.Common.PublishedModels.RichTextFooterRow.GetFooterHeadlineHide(this, _publishedValueFallback);
+
+		///<summary>
+		/// Edit Text: Edit the headline title
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.0.3+a0f3c15")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("editText")]
+		public virtual global::Umbraco.Cms.Core.Strings.IHtmlEncodedString EditText => global::Umbraco.Cms.Web.Common.PublishedModels.RichTextHeadlineRow.GetEditText(this, _publishedValueFallback);
+
+		///<summary>
+		/// Font Size Editor
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.0.3+a0f3c15")]
+		[ImplementPropertyType("fontSizeEditor")]
+		public virtual int FontSizeEditor => global::Umbraco.Cms.Web.Common.PublishedModels.RichTextHeadlineRow.GetFontSizeEditor(this, _publishedValueFallback);
+
+		///<summary>
+		/// Hide: Select to true if you need to hide the text
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.0.3+a0f3c15")]
+		[ImplementPropertyType("hide")]
+		public virtual bool Hide => global::Umbraco.Cms.Web.Common.PublishedModels.RichTextHeadlineRow.GetHide(this, _publishedValueFallback);
+
+		///<summary>
+		/// Select Color
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.0.3+a0f3c15")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("selectColor")]
+		public virtual string SelectColor => global::Umbraco.Cms.Web.Common.PublishedModels.RichTextHeadlineRow.GetSelectColor(this, _publishedValueFallback);
+
+		///<summary>
+		/// Hide Search Combo Contents
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.0.3+a0f3c15")]
+		[ImplementPropertyType("hideSearchComboContents")]
+		public virtual bool HideSearchComboContents => global::Umbraco.Cms.Web.Common.PublishedModels.SearchComboProperties.GetHideSearchComboContents(this, _publishedValueFallback);
+
+		///<summary>
+		/// Search Combo Contents
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.0.3+a0f3c15")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("searchComboContents")]
+		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockListModel SearchComboContents => global::Umbraco.Cms.Web.Common.PublishedModels.SearchComboProperties.GetSearchComboContents(this, _publishedValueFallback);
 	}
 }
