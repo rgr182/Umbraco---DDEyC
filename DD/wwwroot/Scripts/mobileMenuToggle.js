eb04c4aa8e78cc5c28653712840aeabed7aa2bd7
Gibrán Pérez <109172763+Gapcroz@users.noreply.github.com>
2024-05-27 21:46:07 -0600

    update display submenus using the dropdown's arrow

--- DD/wwwroot/Scripts/mobileMenuToggle.js
+++ DD/wwwroot/Scripts/mobileMenuToggle.js
@@ -1,20 +1,17 @@
-﻿const dropdownArrows = document.querySelectorAll('.dropdown-arrow');
-const openMenuButton = document.querySelector('#open-menu');
-const navigationMenu = document.querySelector('#navigation ul');
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const navigationMenu = document.querySelector('#navigation ul');
 
-// Alternar la visibilidad del menú de navegación al hacer clic en el botón de menú
-openMenuButton.addEventListener('click', () => {
-    if (navigationMenu.style.display === 'none' || navigationMenu.style.display === '') {
-        navigationMenu.style.display = 'block';
-    } else {
-        navigationMenu.style.display = 'none';
-    }
-});
+    const toggleNavigationMenu = () => {
+        if (navigationMenu.style.display === 'none' || navigationMenu.style.display === '') {
+            navigationMenu.style.display = 'block';
+        } else {
+            navigationMenu.style.display = 'none';
+        }
+    };
 
-// Alternar la visibilidad de los submenús al hacer clic en el dropdown-arrow
-dropdownArrows.forEach((arrow) => {
-    arrow.addEventListener('click', (e) => {
-        const dropdownMenu = arrow.nextElementSibling;
+    // Alternar la visibilidad de los submenús al hacer clic en el dropdown-arrow
+    document.querySelector('.dropdown-arrow').addEventListener('click', (e) => {
+        const dropdownMenu = e.target.nextElementSibling;
         if (dropdownMenu.style.display === 'block') {
             dropdownMenu.style.display = 'none';
         } else {
@@ -22,28 +19,18 @@ dropdownArrows.forEach((arrow) => {
         }
         e.stopPropagation();
     });
-});
 
-// Ajustar la visibilidad del menú de navegación al redimensionar la ventana
-window.addEventListener('resize', () => {
-    if (window.innerWidth > 970) {
-        navigationMenu.style.display = 'flex';
-    } else {
-        navigationMenu.style.display = 'none';
-    }
-});
+    // Alternar la visibilidad del menú de navegación al hacer clic en el botón de menú
+    document.querySelector('#open-menu').addEventListener('click', () => {
+        toggleNavigationMenu();
+    });
 
-// Ocultar el menú de navegación y los submenús si se hace clic fuera de ellos
-document.addEventListener('click', (e) => {
-    if (window.innerWidth <= 970 && navigationMenu && !e.target.closest('#navigation')) {
-        navigationMenu.style.display = 'none';
-        dropdownArrows.forEach((arrow) => {
-            const dropdownMenu = arrow.nextElementSibling;
-            if (dropdownMenu) {
-                dropdownMenu.classList.remove('active');
-                arrow.classList.remove('active');
-            } 
-        });
-    }
+    // Ajustar la visibilidad del menú de navegación al redimensionar la ventana
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 970) {
+            navigationMenu.style.display = 'flex';
+        } else {
+            navigationMenu.style.display = 'none';
+        }
+    });
 });
-
