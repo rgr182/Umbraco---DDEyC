bed7a723248b3fcbb6ab8f070d4f5fafa76f3fcf
Gibrán Pérez <109172763+Gapcroz@users.noreply.github.com>
2024-05-30 14:27:42 -0600

    update bugfix navigationBar

--- DD/wwwroot/Scripts/mobileMenuToggle.js
+++ DD/wwwroot/Scripts/mobileMenuToggle.js
@@ -1,6 +1,8 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
 
+    // despliega la barra de navegación
     const navigationMenu = document.querySelector('#navigation ul');
+
     const toggleNavigationMenu = () => {
         if (navigationMenu.style.display === 'none' || navigationMenu.style.display === '') {
             navigationMenu.style.display = 'block';
@@ -8,6 +10,12 @@
             navigationMenu.style.display = 'none';
         }
     };
+
+    // Alternar la visibilidad del menú de navegación al hacer clic en el botón de menú
+    document.querySelector('#open-menu').addEventListener('click', () => {
+        toggleNavigationMenu();
+        e.stopPropagation();
+    });
 
     // Alternar la visibilidad de los submenús al hacer clic en el dropdown-arrow
     document.querySelectorAll('.dropdown-arrow').forEach(arrow => {
@@ -22,17 +30,39 @@
         });
     });
 
-    // Alternar la visibilidad del menú de navegación al hacer clic en el botón de menú
-    document.querySelector('#open-menu').addEventListener('click', () => {
-        toggleNavigationMenu();
-    });
-
     // Ajustar la visibilidad del menú de navegación al redimensionar la ventana
     window.addEventListener('resize', () => {
-        if (window.innerWidth > 1010) {
+        if (window.innerWidth >= 1009) {
             navigationMenu.style.display = 'flex';
         } else {
             navigationMenu.style.display = 'none';
         }
     });
+
+    // bloqueo de menús con hijos
+    const toggleDropdown = function (event, dropdownClass) {
+        const parentLi = event.target.closest('li');
+        const dropdown = parentLi.querySelector(dropdownClass);
+
+        if (dropdown) {
+            event.preventDefault();
+            const isOpen = dropdown.classList.toggle('open');
+            event.target.setAttribute('aria-expanded', isOpen);
+        }
+    };
+
+    const setupLinks = function (linkSelector, dropdownClass) {
+        const links = document.querySelectorAll(linkSelector);
+
+        links.forEach(function (link) {
+            link.addEventListener('click', function (event) {
+                toggleDropdown(event, dropdownClass);
+            });
+            link.setAttribute('aria-haspopup', 'true');
+            link.setAttribute('aria-expanded', 'false');
+        });
+    };
+
+    setupLinks('.parent-link', '.dropdown');
+    setupLinks('.child-link', '.sub-dropdown');
 });
