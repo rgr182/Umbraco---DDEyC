bd162c60ebe0a4fc03e587d93b11d154f9c02d1b
Gibrán Pérez <109172763+Gapcroz@users.noreply.github.com>
2024-05-29 18:47:01 -0600

    update navigation bar

--- DD/wwwroot/Scripts/mobileMenuToggle.js
+++ DD/wwwroot/Scripts/mobileMenuToggle.js
@@ -1,6 +1,6 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
-    const navigationMenu = document.querySelector('#navigation ul');
 
+    const navigationMenu = document.querySelector('#navigation ul');
     const toggleNavigationMenu = () => {
         if (navigationMenu.style.display === 'none' || navigationMenu.style.display === '') {
             navigationMenu.style.display = 'block';
@@ -10,14 +10,16 @@
     };
 
     // Alternar la visibilidad de los submenús al hacer clic en el dropdown-arrow
-    document.querySelector('.dropdown-arrow').addEventListener('click', (e) => {
-        const dropdownMenu = e.target.nextElementSibling;
-        if (dropdownMenu.style.display === 'block') {
-            dropdownMenu.style.display = 'none';
-        } else {
-            dropdownMenu.style.display = 'block';
-        }
-        e.stopPropagation();
+    document.querySelectorAll('.dropdown-arrow').forEach(arrow => {
+        arrow.addEventListener('click', (e) => {
+            const dropdownMenu = e.target.nextElementSibling;
+            if (dropdownMenu.style.display === 'block') {
+                dropdownMenu.style.display = 'none';
+            } else {
+                dropdownMenu.style.display = 'block';
+            }
+            e.stopPropagation();
+        });
     });
 
     // Alternar la visibilidad del menú de navegación al hacer clic en el botón de menú
@@ -27,7 +29,7 @@
 
     // Ajustar la visibilidad del menú de navegación al redimensionar la ventana
     window.addEventListener('resize', () => {
-        if (window.innerWidth > 970) {
+        if (window.innerWidth > 1010) {
             navigationMenu.style.display = 'flex';
         } else {
             navigationMenu.style.display = 'none';
