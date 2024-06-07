ead5bb6ec25e5e0efe6524928b9655ff62d62625
Gibrán Pérez <109172763+Gapcroz@users.noreply.github.com>
2024-06-07 15:14:08 -0600

    update DDCM-22

--- DD/umbraco/models/GeneralContentPage.generated.cs
+++ DD/umbraco/models/GeneralContentPage.generated.cs
@@ -20,7 +20,7 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 {
 	/// <summary>General Content Page</summary>
 	[PublishedModel("generalContentPage")]
-	public partial class GeneralContentPage : PublishedContentModel, IHeaderProperties
+	public partial class GeneralContentPage : PublishedContentModel, IFooterProperties, IHeaderProperties
 	{
 		// helpers
 #pragma warning disable 0109 // new is redundant
