2f75b6cc8199f9f66570c7725c5fe44a23840d0e
Gibrán Pérez <109172763+Gapcroz@users.noreply.github.com>
2024-06-19 11:46:45 -0600

    update activity DDCM-26 adjusted modal solution

--- DD/wwwroot/Scripts/modalImageCarousel.js
+++ DD/wwwroot/Scripts/modalImageCarousel.js
@@ -3,20 +3,26 @@
     var modalImg = document.getElementById("modalImage");
     var carouselImages = document.querySelectorAll('.carousel-item img');
     var span = document.getElementsByClassName("modal-close")[0];
+
     carouselImages.forEach(function (img) {
         img.onclick = function () {
             if (window.innerWidth <= 767) {
                 modal.style.display = "block";
                 modalImg.src = this.src;
+                document.body.style.overflow = "hidden";
             }
         }
     });
+
     span.onclick = function () {
         modal.style.display = "none";
+        document.body.style.overflow = "auto";
     }
+
     modal.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
+            document.body.style.overflow = "auto";
         }
     }
 });
