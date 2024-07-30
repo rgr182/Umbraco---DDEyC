8422676d2e8ede4482961031b432b27baba6985e
Gibrán Pérez <109172763+Gapcroz@users.noreply.github.com>
2024-07-30 15:58:21 -0600

    google Form In Landing Page

---
+++ DD/wwwroot/Scripts/googleForm.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var toggleButton = document.getElementById('toggleButton');
+    var formDiv = document.getElementById('formDiv');
+    var googleFormIframe = document.getElementById('googleFormIframe');
+
+    // Apply Bootstrap classes to iframe
+    googleFormIframe.style.width = '100%';
+    googleFormIframe.style.height = '600px'; // Adjust as necessary
+
+    toggleButton.addEventListener('click', function () {
+        if (formDiv.classList.contains('d-none')) {
+            formDiv.classList.remove('d-none');
+            formDiv.classList.add('d-block');
+        } else {
+            formDiv.classList.remove('d-block');
+            formDiv.classList.add('d-none');
+        }
+    });
+});
